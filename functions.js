1ba984ab024fe1ed7f37c38d8e623c71aef98bbb
Brenda Rychter <brenrychter@Brendas-MacBook-Pro.local>
2016-04-24 21:33:24 -0300

    functions prj

---
+++ functions.js
@@ -0,0 +1,41 @@
+function verifyNetwork(connection){
+	if(connection){
+		//stuff
+		return true;
+	}
+}
+
+
+function refreshNews(conexion){
+	if(verifyNetwork){
+		//connect to database.
+		loadWall();
+		receiveNotifications();
+	}else{
+		//show the data existing before
+		alertNoConnection();
+	}
+}
+function loadWall(){
+	
+}
+
+function receiveNotifications(){
+	
+}
+
+function publishContent(){
+	var action = //takePicture(); takeVideo();
+	if(verifyNetwork){
+		//send data to server
+		sendNotification(action, username);
+	}
+}
+
+function sendNotification(action, username){
+
+}
+
+function alertNoConnection(){
+	alert('connection is missed');
+}
